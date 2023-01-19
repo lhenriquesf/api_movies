0805257862a65562847e10c03e9f4038120eb5a3
luiz henrique <franca.luiz1@hotmail.com>
2023-01-18 23:36:09 -0300

    validacao de email e senha

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,8 @@ const {Router} = require("express");
 const usersRoutes = require("./users.routes");
 const notesRoutes = require("./notes.routes");
 const tagsRoutes = require("./tags.routes");
+const sessionsRoutes = require("./sessions.routes");
+
 
 
 const routes = Router();
@@ -10,6 +12,7 @@ const routes = Router();
 routes.use("/users", usersRoutes);
 routes.use("/notes", notesRoutes);
 routes.use("/tags", tagsRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 
 module.exports = routes;
